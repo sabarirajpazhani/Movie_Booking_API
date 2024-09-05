46065d85e7cfd824ffdc56c752033134a9f91b46
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-09-05 11:16:47 +0530

    API Testing ensure

--- backend/App.js
+++ backend/App.js
@@ -4,6 +4,14 @@ const dotenv = require("dotenv");
 const path = require('path');
 dotenv.config({path:path.join(__dirname,'config','config.env') })
 
+const movies = require('./routes/movies');
+const booking = require('./routes/booking');
+
+app.use(express.json());
+
+app.use('/api/v1/',movies);
+app.use('/api/v2/',booking);
+
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is Listening to Port ${process.env.PORT} in ${process.env.NODE_ENV}`);
 })
--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { getBooking, getBookingCancel } = require("../controllers/bookingControllers");
-const route = express.Router();
+const router = express.Router();
 
 router.route('/movies/:id/book').post(getBooking);
-router.rpute('/movies/:id/cancel').post(getBookingCancel)
+router.route('/movies/:id/cancel').post(getBookingCancel);
+
+module.exports=router;
